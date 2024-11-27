26c53d7a85345c369980281d9b310eb8f82414b0
Cadu Nunes <109031414+cadu-nunes@users.noreply.github.com>
2024-11-26 21:10:59 -0300

    Add files via upload

---
+++ eventos.js
@@ -0,0 +1,28 @@
+
+// Seleciona os elementos
+const caixa = document.getElementById("caixa");
+const botao = document.getElementById("botao");
+
+// Adiciona um evento de clique no botão
+botao.addEventListener("mouseover", () => {
+    // Verifica se a caixa tem a classe "visivel"
+    if (caixa.classList.contains("visivel")) {
+        // Troca a classe para "oculto"
+        caixa.classList.replace("visivel", "oculto");
+    } else {
+        // Troca a classe para "visivel"
+        caixa.classList.replace("oculto", "visivel");
+    }
+});
+
+// Adiciona um evento de clique no botão
+botao.addEventListener("mouseout", () => {
+    // Verifica se a caixa tem a classe "visivel"
+    if (caixa.classList.contains("visivel")) {
+        // Troca a classe para "oculto"
+        caixa.classList.replace("visivel", "oculto");
+    } else {
+        // Troca a classe para "visivel"
+        caixa.classList.replace("oculto", "visivel");
+    }
+});
